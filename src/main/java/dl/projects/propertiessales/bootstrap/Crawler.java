2a858a70f91606127bc69e17a74192581592ec3c
Doronlnd84 <doronlnd84@gmail.com>
2019-08-08 08:17:20 +0300

    add crowler

---
+++ src/main/java/dl/projects/propertiessales/bootstrap/Crawler.java
@@ -0,0 +1,30 @@
+package dl.projects.propertiessales.bootstrap;
+
+import java.util.List;
+
+public class Crawler {
+
+   private int GetLastPage(String websiteUrl)
+   {
+
+       return -1;
+   }
+    private List<String> GetPageUrlList(int lastPage)
+    {
+
+        return null;
+    }
+
+    private String GetPropertyUrl(String pageUrl)
+    {
+        return null;
+    }
+    private String ExtractData(String propertyUrl)
+    {
+        return null;
+    }
+
+
+
+
+}
